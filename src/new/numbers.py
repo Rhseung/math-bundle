f94bbff4a1e909cca3ce661cb9c4fa38be89abd1
hapdong <56152093+Rhseung@users.noreply.github.com>
2022-04-29 08:32:38 +0900

    Update numbers.py

--- src/new/numbers.py
+++ src/new/numbers.py
@@ -1,5 +1,3 @@
-3*x + 1
-
 class Rational:
     n = 0
     d = 1
@@ -25,18 +23,44 @@ class Rational:
             self.n = int(args[0])
             self.d = int(args[1])
     
-    def __add__(self, other):
-    def __sub__(self, other):        
-    def __mul__(self, other):
-    
-    '''
-    gcd(21, 14):
-    21 14
-    14 21%14=7
-    7 14%7=0
-    '''
+    def __neg__(self):
+        return Rational(-self.n, self.d)
 
-    (a, b) = (n, d)
+    def __invert__(self):
+        return Rational(self.d, self.n)
+
+    def __abs__(self):
+        return abs(self.n / self.d)
+
+    def __add__(self, other):
+        return Rational(
+            self.n * other.d + other.n * self.d, 
+            self.d * other.d
+        )
+
+    def __sub__(self, other):        
+        return self + -other
+
+    def __mul__(self, other):
+        return Rational(self.n * other.n, self.d * other.d)
+    
+    def __truediv__(self, other):
+        return self * ~other
+    
+    def __pow__(self, other: int):
+        if other >= 0:
+            return Rational(self.n ** other, self.d ** other)
+        else:
+            return ~self ** -other
+
+    def __str__(self):
+        return f'{self.n}/{self.d}'
+
+    if (n >= 0 and d <= 0) or (n <= 0 and d <= 0):
+        n = -n
+        d = -d
+
+    (a, b) = (abs(n), abs(d))
     while d != 0:
         tmp = b
         b = a % tmp
@@ -46,4 +70,9 @@ class Rational:
     n /= g
     d /= g
 
-class Expr:
+a = Rational(3, 4)
+b = Rational(1, 7)
+c = Rational(8, -12)
+print(a, b, c)
+
+# class Expr:
